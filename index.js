9e40afe6211d91c73b28a15c16a312b61ac2f7d4
Mjdeleon8 <144708611+Mjdeleon8@users.noreply.github.com>
2024-02-16 22:19:53 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,57 @@
+// let button = document.querySelectorAll(".style");
+// let firstLetter = document.getElementsByClassName(".style ");
+// let seven = document.getElementsByClassName(".seven");
+// let four = document.getElementsByClassName(".four");
+// let one = document.getElementsByClassName(".one");
+// let twoZero = document.getElementsByClassName(".twozero");
+// let percentage = document.getElementsByClassName(".percentage");
+// let eight = document.getElementsByClassName(".eight");
+// let five = document.getElementsByClassName(".five");
+// let two = document.getElementsByClassName(".two");
+// let zero = document.getElementsByClassName(".zero");
+// let ekIs = document.getElementsByClassName(".ekIs");
+// let nine = document.getElementsByClassName(".nine");
+// let six = document.getElementsByClassName(".six");
+// let three = document.getElementsByClassName(".three");
+// let dot = document.getElementsByClassName(".dot");
+// let divide = document.getElementsByClassName(".divide");
+// let times = document.getElementsByClassName(".times");
+// let minus = document.getElementsByClassName(".minus");
+// let plus = document.getElementsByClassName(".plus");
+// let equal = document.getElementsByClassName(".equal");
+
+// for (let i = 0; i < button.length; i++) {
+//   button[i].addEventListener("click", function () {
+//     console.log("hello");
+//   });
+// }
+
+// firstLetter.addEventListener("click", () => {
+//   console.log("hello");
+// }
+// const allButtons = document.querySelectorAll("elements");
+
+// allButtons.forEach("click", () => {
+//   console.log("hello");
+// });
+
+let show = document.getElementById("display");
+
+function calculatorFunction(input) {
+  show.value += input;
+}
+
+function displayClear() {
+  show.value = "";
+}
+
+function calculateFunction() {
+  let input = document.getElementById("display").value;
+
+  try {
+    let result = new Function("return " + input)();
+    document.getElementById("display").value = result;
+  } catch (error) {
+    document.getElementById("display").value = "Error";
+  }
+}
